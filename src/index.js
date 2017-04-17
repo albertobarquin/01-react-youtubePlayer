3294fb9743f6cbd4b4c539e1234a48aba24d2da6
alberto barquin <alberto@barquin.es>
2017-04-17 12:33:56 +0200

    section 2 lecture 31

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDom from 'react-dom';
 
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+
 
 import YTSearch from 'youtube-api-search';
 
@@ -13,8 +15,20 @@ class App extends Component{
     constructor (props){
         super (props);
 
-        this.state = {videos:[]};
-        YTSearch( {key:API_KEY, term:'Juego de tronos'}, (videos) => this.setState({videos}));
+        this.state = {
+            videos:[],
+            selectedVideo: null
+
+        };
+        YTSearch( {key:API_KEY, term:'Juego de tronos'}, (videos) =>
+
+            this.setState({
+
+                videos: videos,
+                selectedVideo: videos[0]
+
+
+            }));
 
     }
 
@@ -22,7 +36,11 @@ class App extends Component{
         return(
             <div>
                 <SearchBar />
-                <VideoList videos={this.state.videos}/>
+                <VideoDetail video = {this.state.selectedVideo}/>
+                <VideoList
+                    onVideoSelect = {selectedVideo => this.setState({selectedVideo})}
+                    videos={this.state.videos}
+                />
             </div>
         );
     }
